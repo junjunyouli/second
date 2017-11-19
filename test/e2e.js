5d1b7f5b04c8c29684759122b24adb40808d5672
zhu.jun <zhu.jun@datatom.com>
2017-11-19 13:53:09 +0800

    1

--- test/e2e.js
+++ test/e2e.js
@@ -8,4 +8,5 @@ driver.get('http://localhost:3000/index/index');
 driver.findElement(By.id('hands')).click();
 const _animation = driver.findElement(By.id('add'));
 driver.wait(_animation.isDisplayed(),1000);
-// driver.quit();
+driver.sleep(5000);
+driver.quit();
